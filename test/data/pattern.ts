ea0b042a2bc221a4534682d21f277a94d9aff220
dov118 <contact@dov118.dev>
2024-09-09 09:17:20 +0200

    Related Feature v2

--- test/data/pattern.ts
+++ test/data/pattern.ts
@@ -5276,6 +5276,44 @@ const pattern: PatternItem[] = [
       },
     ],
   },
+  {
+    date: '2024-09-06T16_32_21',
+    url: ForumMessagePTSURL,
+    file: '2024-09-06T16_32_21.html',
+    rawList: ['The maintenance is complete, and the PTS is now available.'],
+    expected: [
+      {
+        source: ForumMessagePTSURL,
+        raw: 'The maintenance is complete, and the PTS is now available.',
+        slug: ServerPcPtsSlug,
+        type: ServerType,
+        support: PcSupport,
+        zone: PtsZone,
+        status: UpStatus,
+        rawSlug: 'PTS',
+        rawStatus: 'now available',
+      },
+    ],
+  },
+  {
+    date: '2024-09-06T19_00_34',
+    url: ForumMessagePTSURL,
+    file: '2024-09-06T19_00_34.html',
+    rawList: ['The maintenance is complete, and the PTS is now available.'],
+    expected: [
+      {
+        source: ForumMessagePTSURL,
+        raw: 'The maintenance is complete, and the PTS is now available.',
+        slug: ServerPcPtsSlug,
+        type: ServerType,
+        support: PcSupport,
+        zone: PtsZone,
+        status: UpStatus,
+        rawSlug: 'PTS',
+        rawStatus: 'now available',
+      },
+    ],
+  },
 ];
 
 export default pattern;
