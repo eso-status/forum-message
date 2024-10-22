8aca920f7736d8c66c27771857b7dcdaf339f76b
dov118 <contact@dov118.dev>
2024-10-22 11:25:11 +0200

    Related Feature v2

--- test/data/pattern20241014.ts
+++ test/data/pattern20241014.ts
@@ -56,10 +56,10 @@ const pattern20241014: PatternItem[] = [
     url: ForumMessageURL,
     file: '2024-10-14T10_32_27.html',
     rawList: [
-      '• [IN PROGRESS] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 4:00PM EDT (20:00 UTC)', // Down et maintenance en même temps
+      '• [IN PROGRESS] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 4:00PM EDT (20:00 UTC)',
       '• NA megaservers for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
       '• EU megaservers for maintenance – October 16, 8:00 UTC (4:00AM EDT) - 16:00 UTC (12:00PM EDT)',
-      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)', // Down et maintenance en même temps
+      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
     ],
     expected: [
       {
@@ -192,10 +192,10 @@ const pattern20241014: PatternItem[] = [
     file: '2024-10-14T10_33_38.html',
     rawList: [
       'We will be performing maintenance for patch 10.2.5 on the PTS on Monday at 10:00AM EDT (14:00 UTC).',
-      '• [IN PROGRESS] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 4:00PM EDT (20:00 UTC)', // Down et maintenance en même temps
+      '• [IN PROGRESS] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 4:00PM EDT (20:00 UTC)',
       '• NA megaservers for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
       '• EU megaservers for maintenance – October 16, 8:00 UTC (4:00AM EDT) - 16:00 UTC (12:00PM EDT)',
-      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)', // Down et maintenance en même temps
+      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
     ],
     expected: [
       {
@@ -340,10 +340,10 @@ const pattern20241014: PatternItem[] = [
     url: ForumMessagePTSURL,
     file: '2024-10-14T14_01_44.html',
     rawList: [
-      '• [IN PROGRESS] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 4:00PM EDT (20:00 UTC)', // Down et maintenance en même temps
+      '• [IN PROGRESS] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 4:00PM EDT (20:00 UTC)',
       '• NA megaservers for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
       '• EU megaservers for maintenance – October 16, 8:00 UTC (4:00AM EDT) - 16:00 UTC (12:00PM EDT)',
-      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)', // Down et maintenance en même temps
+      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
       'The PTS is now offline for the patch 10.2.5 maintenance and is currently unavailable.',
     ],
     expected: [
@@ -487,12 +487,12 @@ const pattern20241014: PatternItem[] = [
     url: ForumMessagePTSURL,
     file: '2024-10-14T18_25_48.html',
     rawList: [
-      'We will be performing maintenance on the PTS on Wednesday at 4:00AM EDT (8:00 UTC).', // Up et maintenance en même temps
-      '• [IN PROGRESS] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 4:00PM EDT (20:00 UTC)', // Down et maintenance en même temps
+      'We will be performing maintenance on the PTS on Wednesday at 4:00AM EDT (8:00 UTC).',
+      '• [IN PROGRESS] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 4:00PM EDT (20:00 UTC)',
       '• NA megaservers for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
       '• EU megaservers for maintenance – October 16, 8:00 UTC (4:00AM EDT) - 16:00 UTC (12:00PM EDT)',
-      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)', // Down et maintenance en même temps
-      'The maintenance is complete, and the PTS is now back online and patch 10.2.5 is available.', // Up et maintenance en même temps
+      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
+      'The maintenance is complete, and the PTS is now back online and patch 10.2.5 is available.',
     ],
     expected: [
       {
@@ -648,12 +648,12 @@ const pattern20241014: PatternItem[] = [
     url: ForumMessagePTSURL,
     file: '2024-10-14T19_57_29.html',
     rawList: [
-      'We will be performing maintenance on the PTS on Wednesday at 4:00AM EDT (8:00 UTC).', // Up et maintenance en même temps
-      '• [EXTENDED] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 6:00PM EDT (22:00 UTC)', // Down et maintenance en même temps
+      'We will be performing maintenance on the PTS on Wednesday at 4:00AM EDT (8:00 UTC).',
+      '• [EXTENDED] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 6:00PM EDT (22:00 UTC)',
       '• NA megaservers for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
       '• EU megaservers for maintenance – October 16, 8:00 UTC (4:00AM EDT) - 16:00 UTC (12:00PM EDT)',
-      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)', // Down et maintenance en même temps
-      'The maintenance is complete, and the PTS is now back online and patch 10.2.5 is available.', // Up et maintenance en même temps
+      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
+      'The maintenance is complete, and the PTS is now back online and patch 10.2.5 is available.',
     ],
     expected: [
       {
@@ -809,10 +809,10 @@ const pattern20241014: PatternItem[] = [
     url: ForumMessageURL,
     file: '2024-10-14T19_59_12.html',
     rawList: [
-      '• [EXTENDED] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 6:00PM EDT (22:00 UTC)', // Down et maintenance en même temps
+      '• [EXTENDED] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 6:00PM EDT (22:00 UTC)',
       '• NA megaservers for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
       '• EU megaservers for maintenance – October 16, 8:00 UTC (4:00AM EDT) - 16:00 UTC (12:00PM EDT)',
-      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)', // Down et maintenance en même temps
+      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
     ],
     expected: [
       {
@@ -944,12 +944,12 @@ const pattern20241014: PatternItem[] = [
     url: ForumMessagePTSURL,
     file: '2024-10-14T21_45_42.html',
     rawList: [
-      'We will be performing maintenance on the PTS on Wednesday at 4:00AM EDT (8:00 UTC).', // Up et maintenance en même temps
-      'The maintenance is complete, and the PTS is now back online and patch 10.2.5 is available.', // Up et maintenance en même temps
-      '• [COMPLETE] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 6:00PM EDT (22:00 UTC)', // Up et maintenance en même temps
+      'We will be performing maintenance on the PTS on Wednesday at 4:00AM EDT (8:00 UTC).',
+      'The maintenance is complete, and the PTS is now back online and patch 10.2.5 is available.',
+      '• [COMPLETE] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 6:00PM EDT (22:00 UTC)',
       '• NA megaservers for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
       '• EU megaservers for maintenance – October 16, 8:00 UTC (4:00AM EDT) - 16:00 UTC (12:00PM EDT)',
-      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)', // Up et maintenance en même temps
+      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
     ],
     expected: [
       {
@@ -1105,10 +1105,10 @@ const pattern20241014: PatternItem[] = [
     url: ForumMessageURL,
     file: '2024-10-14T21_46_45.html',
     rawList: [
-      '• [COMPLETE] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 6:00PM EDT (22:00 UTC)', // Up et maintenance en même temps
+      '• [COMPLETE] ESO Store and Account System for maintenance – October 14, 6:00AM EDT (10:00 UTC) - 6:00PM EDT (22:00 UTC)',
       '• NA megaservers for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
       '• EU megaservers for maintenance – October 16, 8:00 UTC (4:00AM EDT) - 16:00 UTC (12:00PM EDT)',
-      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)', // Up et maintenance en même temps
+      '• ESO Store and Account System for maintenance – October 16, 4:00AM EDT (8:00 UTC) - 12:00PM EDT (16:00 UTC)',
     ],
     expected: [
       {
