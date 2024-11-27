26c0a2a0798081d34aac1c96601b71d663b0cc86
dov118 <contact@dov118.dev>
2024-11-27 14:01:36 +0100

    Related Feature v2

--- src/type/remoteServerXboxEuRawSlug.type.ts
+++ src/type/remoteServerXboxEuRawSlug.type.ts
@@ -5,4 +5,5 @@ export type RemoteServerXboxEuRawSlug =
   | 'Xbox: NA and EU megaservers for'
   | 'Xbox: EU megaserver for'
   | '] EU megaservers for'
-  | '• EU megaservers for';
+  | '• EU megaservers for'
+  | 'Xbox Live™';
--- test/data/pattern.ts
+++ test/data/pattern.ts
@@ -42,6 +42,9 @@ import pattern20241101 from './pattern20241101';
 import pattern20241104 from './pattern20241104';
 // import pattern20241106 from './pattern20241106';
 import pattern20241108 from './pattern20241108';
+import pattern20241111 from './pattern20241111';
+import pattern20241113 from './pattern20241113';
+import pattern20241114 from './pattern20241114';
 
 const pattern: PatternItem[] = [
   ...pattern20240814,
@@ -87,6 +90,9 @@ const pattern: PatternItem[] = [
   ...pattern20241104,
   // ...pattern20241106,
   ...pattern20241108,
+  ...pattern20241111,
+  ...pattern20241113,
+  ...pattern20241114,
 ];
 
 export default pattern;
